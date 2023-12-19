fcdb0ecc325fd690cbc01604b845f753b75e9a82
Bouali-Salma <salmabouali2610@gmail.com>
2023-12-19 21:44:43 +0100

    Tips Api (Read)

--- app/src/main/java/com/groupe6/babycare/repositories/apis/TipsApi.java
+++ app/src/main/java/com/groupe6/babycare/repositories/apis/TipsApi.java
@@ -1,7 +1,19 @@
 package com.groupe6.babycare.repositories.apis;
 
+import com.groupe6.babycare.dtos.tips.TipsDTO;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface TipsApi {
 
 // TODO: Add all methods for TipsApi
+    @GET("/api/v1/tips")
+    Call<List<TipsDTO>> getAll();
 
+    @GET("/api/v1/tips/{id}")
+    Call<TipsDTO> getNoteById(@Path("id") Long id);
 }
--- app/src/main/java/com/groupe6/babycare/repositories/implementations/TipsApiImpl.java
+++ app/src/main/java/com/groupe6/babycare/repositories/implementations/TipsApiImpl.java
@@ -1,6 +1,73 @@
 package com.groupe6.babycare.repositories.implementations;
 
+import android.content.Context;
+
+import com.groupe6.babycare.dtos.error.ErrorDTO;
+import com.groupe6.babycare.dtos.tips.TipsDTO;
+import com.groupe6.babycare.listeners.ResponseListener;
+import com.groupe6.babycare.repositories.ApiClient;
+import com.groupe6.babycare.repositories.apis.TipsApi;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class TipsApiImpl {
+
     // TODO: Add implementation for TipsApi
+    private static TipsApiImpl tipsApiImpl;
+    private static TipsApi tipsApi;
+
+    public TipsApiImpl() {
+    }
+
+    public static TipsApiImpl getInstance(Context context) {
+        if (tipsApi == null) {
+            tipsApi = ApiClient.getClient(context).create(TipsApi.class);
+            tipsApiImpl = new TipsApiImpl();
+        }
+        return tipsApiImpl;
+    }
+
+
+      public void getAllTips(final ResponseListener<List<TipsDTO>> listener) {
+        tipsApi.getAll().enqueue(new Callback<List<TipsDTO>>() {
+            @Override
+            public void onResponse(Call<List<TipsDTO>> call, Response<List<TipsDTO>> response) {
+                if (response.isSuccessful()) {
+                    listener.onSuccess(response.body());
+                } else {
+                    ErrorDTO errorDTO = new ErrorDTO(response.message(), response.code());
+                    listener.onError(errorDTO);
+                }
+            }
+            @Override
+            public void onFailure(Call<List<TipsDTO>> call, Throwable t) {
+                // Handle failure
+            }
+        });
+    }
+
+    public void getTip(Long id, final  ResponseListener<TipsDTO> listener){
+        tipsApi.getNoteById(id).enqueue(new Callback<TipsDTO>() {
+            @Override
+            public void onResponse(Call<TipsDTO> call, Response<TipsDTO> response) {
+                if(response.isSuccessful()){
+                    listener.onSuccess(response.body());
+                }
+                else {
+                    ErrorDTO errorDTO = new ErrorDTO(response.message(), response.code());
+                    listener.onError(errorDTO);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<TipsDTO> call, Throwable t) {
+
+            }
+        });
+    }
 
 }
